dc253ad5a5141038b2d559d7a903ef66fe1d66c5
Abdullah Korayem <abdullah0562007@gmail.com>
2024-05-15 06:24:44 +0300

    change some errors

--- components/HeaderDialog.tsx
+++ components/HeaderDialog.tsx
@@ -1,54 +1,56 @@
 
 interface HeaderDialogProps {
-  mobileMenuOpen: boolean,
-  setMobileMenuOpen: (isOpen: boolean) => void
+    mobileMenuOpen: boolean,
+    setMobileMenuOpen: (isOpen: boolean) => void
 }
 
 import { Button } from "@/components/ui/button"
 import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
 import DialogCountries from "./DialogCountries";
+import { AlignJustify } from "lucide-react";
 
 export default function HeaderDialog({ mobileMenuOpen, setMobileMenuOpen }: HeaderDialogProps) {
 
 
-  return (
-    <div className="lg:hidden">
+    return (
+        <div className="lg:hidden">
 
 
 
-      <Dialog
-        open={mobileMenuOpen}
-        onOpenChange={setMobileMenuOpen}>
-        <DialogTrigger asChild>
-          <Button variant="outline" className="bg-transparent outline-none border-none hover:bg-transparent">  <SWMIcon name="MenuHamburger" set="broken" color="white" /></Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-md  border-none rounded-lg">
-          <DialogHeader>
-            <DialogTitle className="font-semibold text-lg  sm:text-center"> More</DialogTitle>
-            <DialogDescription >
+            <Dialog
+                open={mobileMenuOpen}
+                onOpenChange={setMobileMenuOpen}>
+                <DialogTrigger asChild>
+                    <Button variant="outline" className="bg-transparent outline-none border-none hover:bg-transparent">  
+                        <AlignJustify  color="white" /></Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-md  border-none rounded-lg">
+                    <DialogHeader>
+                        <DialogTitle className="font-semibold text-lg  sm:text-center"> More</DialogTitle>
+                        <DialogDescription >
 
-            </DialogDescription>
-          </DialogHeader>
-          <div className="flex items-center space-x-2">
-            <div className="grid flex-1 gap-2 overflow-y-auto">
-              <DialogCountries />
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div className="flex items-center space-x-2">
+                        <div className="grid flex-1 gap-2 overflow-y-auto">
+                            <DialogCountries />
 
-            </div>
+                        </div>
 
-          </div>
-          <DialogFooter className="sm:justify-start">
-            <DialogClose asChild>
-              <Button type="button" variant="secondary">
-                Close
-              </Button>
-            </DialogClose>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
+                    </div>
+                    <DialogFooter className="sm:justify-start">
+                        <DialogClose asChild>
+                            <Button type="button" variant="secondary">
+                                Close
+                            </Button>
+                        </DialogClose>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
 
 
 
-    </div>
-  )
+        </div>
+    )
 }
 
